60538d0e3baa50f27b5377332a9f1783a108bed7
Prashanth Puranik <puranik@digdeeper.in>
2020-09-01 14:43:11 +0530

    middleware stack behavior

--- 06-nodejs/19-express-app/index.js
+++ 06-nodejs/19-express-app/index.js
@@ -4,6 +4,19 @@ const indexRouter = require( './routes/index' );
 // Express Application object
 const app = express();
 
+// this middleware function is called for any GET request
+// app.use( route ) -> the route is matched if a prefix of he URL matches the route
+app.use( '/', ( req, res, next ) => {
+    console.log( 'Received req at', (new Date()).toTimeString() );
+    
+    console.log( 'fn 1' );
+    // control transferred to next midlleware
+    next();
+
+    // when response goes out this is where the function resumes
+    console.log( 'Response sent at', (new Date()).toTimeString() );
+});
+
 app.use( indexRouter );
 
 const PORT = process.env.PORT || 3000;
--- 06-nodejs/19-express-app/routes/index.js
+++ 06-nodejs/19-express-app/routes/index.js
@@ -5,6 +5,13 @@ const router = express.Router();
 // req, res are enhanced Node.js objects for request and response
 // router object has same routing functions that application object has
 router.get( '/', ( req, res ) => {
+    console.log( 'fn 2' );
+    res.send( 'Hello Express!' );
+    console.log( 'fn2 has sent response' );
+});
+
+router.get( '/', ( req, res ) => {
+    console.log( 'fn 3' );
     res.send( 'Hello Express!' );
 });
 
