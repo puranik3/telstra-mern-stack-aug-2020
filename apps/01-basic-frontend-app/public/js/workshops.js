a0792c7d849816fc881b6a634e6fddf40c179140
Prashanth Puranik <puranik@digdeeper.in>
2020-09-07 18:13:15 +0530

    frontend works

--- apps/01-basic-frontend-app/public/js/workshops.js
+++ apps/01-basic-frontend-app/public/js/workshops.js
@@ -1,17 +1,2 @@
-"use strict";
-fetch("http://workshops-server.herokuapp.com/workshops")
-    .then(function (response) {
-    console.log('hello!');
-    return response.json();
-})
-    .then(function (workshops) {
-    console.log(workshops);
-    var tbody = document.querySelector('tbody');
-    // go through workshops array and form HTML within tbody
-    workshops.forEach(function (workshop) {
-        tbody.innerHTML += "<tr>\n                <td>\n                    <img src=\"" + workshop.imageUrl + "\" style=\"width: 200px;\" />\n                </td>\n                <td>" + workshop.name + "</td>\n                <td>" + workshop.location.city + "</td>\n            </tr>";
-    });
-})
-    .catch(function (error) {
-    console.log(error.message);
-});
+import { fetchAndShowWorkshops } from './services/workshops.js';
+fetchAndShowWorkshops();
---
+++ apps/01-basic-frontend-app/src/services/workshops.ts
@@ -0,0 +1,27 @@
+import IWorkshop from '../models/IWorkshop';
+
+export function fetchAndShowWorkshops() {
+    fetch( `http://workshops-server.herokuapp.com/workshops` )
+        .then(function( response : Response ) {
+            return response.json();
+        })
+        .then(function( workshops : IWorkshop[] ) {
+            console.log( workshops );
+            
+            const tbody = <HTMLElement> document.querySelector( 'tbody' );
+            
+            // go through workshops array and form HTML within tbody
+            workshops.forEach(function( workshop ) {
+                tbody.innerHTML += `<tr>
+                    <td>
+                        <img src="${workshop.imageUrl}" style="width: 200px;" />
+                    </td>
+                    <td>${workshop.name}</td>
+                    <td>${workshop.location.city}</td>
+                </tr>`;
+            });
+        })
+        .catch(function( error ) {
+            console.log( error.message );
+        });
+}
