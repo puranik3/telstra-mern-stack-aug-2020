f5411f48552e634c701b79144795b70f74b1ecc2
Khanna, Dhruv <dhruvkhanna38@gmail.com>
2021-11-03 03:14:07 +0530

    Updates

---
+++ precache-manifest.efa87f70a67a72ea03e8edfbd53d18cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d4f3ec93426500ee78ccf86aa450c3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cb133cb7e9e46f2224e4",
+    "url": "/my-app/static/css/main.d9d5f2c0.chunk.css"
+  },
+  {
+    "revision": "a020b8f937311e2ca49a",
+    "url": "/my-app/static/js/2.f821d87a.chunk.js"
+  },
+  {
+    "revision": "cb133cb7e9e46f2224e4",
+    "url": "/my-app/static/js/main.b4ad35a7.chunk.js"
+  },
+  {
+    "revision": "b21e6c684876931c4036",
+    "url": "/my-app/static/js/runtime-main.338f4773.js"
+  },
+  {
+    "revision": "ba20fc9a92367e1f7374fb4c9418bf03",
+    "url": "/my-app/static/media/logo.ba20fc9a.png"
+  }
+]);
